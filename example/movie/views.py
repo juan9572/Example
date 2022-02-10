df42a06ccdc1f7ce70b599b7a1f38e15f1ca1e72
juan9572 <jprinconu@eafit.edu.co>
2022-02-10 11:20:40 -0500

    Pongo otro titulo en el template con mi nombre

--- example/movie/views.py
+++ example/movie/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1 style="color:red; font-size:50px; text-decoration:underline;">Welcome to Home</h1>')
+    return HttpResponse('<h1 style="color:red; font-size:50px; text-decoration:underline;">Welcome Mr </h1><h1 style="color:blue; font-size:50px;">Juan Pablo Rincon Usma</h1>')
